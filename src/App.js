a0c20c2fae0076c8f88c121c4889fc3878cefd66
Vivekananda <vivekananda3686@gmail.com>
2024-07-14 11:38:30 +0000

    IPL

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import {Route, Switch, BrowserRouter} from 'react-router-dom'
+
+import Home from './components/Home'
+import NotFound from './components/NotFound'
+import TeamMatches from './components/TeamMatches'
+
+import './App.css'
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/team-matches/:id" component={TeamMatches} />
+        <Route component={NotFound} />
+      </Switch>
+    </BrowserRouter>
+  )
+}
+
+export default App
